5a247e80f0db33bb238389030b0f95ea14222bac
Hoonki <ihk626262@gmail.com>
2021-03-11 09:38:57 +0900

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-architect-certification-289902-32-tfstate"
+    bucket = "architect-certification-289902-32-tfstate"
     prefix = "environments/dev"
   }
 }
